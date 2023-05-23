864b86aa84a14493f3f55b52fde67ae5ea3f2d7d
w568w <1278297578@qq.com>
2023-05-23 14:58:43 +0800

    feat: impl `/transaction` (untested)

--- src/api/transactions.rs
+++ src/api/transactions.rs
@@ -1,12 +1,13 @@
+use crate::utils::jwt::{AllowAdmin, JwtClaims};
+use crate::utils::permission::APermission;
+
 use super::preclude::*;
 
-use super::{PagingRequest};
-use actix_web::{
-    get,
-    web::{Query},
-};
+use super::PagingRequest;
+use actix_web::web::Data;
+use actix_web::{get, web::Query};
 use entity::transaction::GetTransaction;
-
+use sea_orm::{DatabaseConnection, EntityTrait, QuerySelect};
 
 #[p(
     responses(
@@ -15,7 +16,18 @@ use entity::transaction::GetTransaction;
 )]
 #[get("/transaction")]
 pub async fn get_transaction_list(
-    _paging: Query<PagingRequest>,
+    paging: Query<PagingRequest>,
+    _auth: APermission<JwtClaims, AllowAdmin>,
+    db: Data<DatabaseConnection>,
 ) -> AResult<AJson<Vec<GetTransaction>>> {
-    todo!()
+    Ok(AJson(
+        entity::transaction::Entity::find()
+            .limit(paging.page_size)
+            .offset(paging.page * paging.page_size)
+            .all(db.get_ref())
+            .await?
+            .into_iter()
+            .map(Into::into)
+            .collect(),
+    ))
 }
