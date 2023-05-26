afca8a983fdee2d0bffa24132e07a195b80747f3
w568w <1278297578@qq.com>
2023-05-26 11:04:52 +0800

    feat: impl transaction range query

--- src/api/transactions.rs
+++ src/api/transactions.rs
@@ -8,22 +8,41 @@ use super::PagingRequest;
 use actix_web::web::Data;
 use actix_web::HttpResponse;
 use actix_web::{get, web::Query};
+use chrono::NaiveDateTime;
 use entity::transaction::GetTransaction;
-use sea_orm::{DatabaseConnection, EntityTrait};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, QueryTrait};
+use serde::Deserialize;
+use utoipa::IntoParams;
+
+#[derive(Deserialize, IntoParams)]
+pub struct TransactionFilter {
+    pub from: Option<NaiveDateTime>,
+    pub to: Option<NaiveDateTime>,
+    #[serde(flatten)]
+    pub paging: PagingRequest,
+}
 
 #[p(
-    params(PagingRequest),
+    params(TransactionFilter),
     responses(
         (status = OK, description = "Get transaction list successfully", body = [GetTransaction]),
     ),
+    security(("jwt_token" = []))
 )]
 #[get("/transaction")]
 pub async fn get_transaction_list(
-    paging: Query<PagingRequest>,
+    params: Query<TransactionFilter>,
     _auth: APermission<JwtClaims, AllowAdmin>,
     db: Data<DatabaseConnection>,
 ) -> AResult<HttpResponse> {
+    let params = params.into_inner();
     entity::transaction::Entity::find()
-        .paged::<DatabaseConnection, _, GetTransaction>(paging.into_inner(), db.get_ref())
+        .apply_if(params.from, |q, v| {
+            q.filter(entity::transaction::Column::CreatedAt.gt(v))
+        })
+        .apply_if(params.to, |q, v| {
+            q.filter(entity::transaction::Column::CreatedAt.lt(v))
+        })
+        .paged::<DatabaseConnection, _, GetTransaction>(params.paging, db.get_ref())
         .await
 }
