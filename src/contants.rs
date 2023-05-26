89d756101a741ef13a696daf5314cb60375e160e
w568w <1278297578@qq.com>
2023-05-26 11:11:19 +0800

    style: clippy fix

--- src/contants.rs
+++ src/contants.rs
@@ -1,14 +1,15 @@
-pub mod user_type{
+pub mod user_type {
     pub const ADMIN: &str = "admin";
     pub const SUPER_ADMIN: &str = "super_admin";
 }
 
-pub mod envs{
+pub mod envs {
     pub const DB_URL: &str = "DB_URL";
     pub const REDIS_URL: &str = "REDIS_URL";
     pub const JWT_SECRET: &str = "JWT_SECRET";
     pub const ALLOW_ALL_CORS: &str = "ALLOW_ALL_CORS";
 }
+
 pub const SECRET_KEY_LENGTH: usize = 32;
 pub const ISSUER: &str = "mid";
 pub const ACCESS_TOKEN_EXPIRE_SECONDS: i64 = 1800;
